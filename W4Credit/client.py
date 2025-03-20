737e3dc25c6579e50084844fe6fc6c72d431b043
Vinh Nguyen <ngtv0404@gmail.com>
2025-03-21 10:35:27 +1100

    implement timeout

--- W4Credit/client.py
+++ W4Credit/client.py
@@ -1,4 +1,5 @@
 from socket import *
+import os
 
 # server IP address and port
 SERVER_NAME = '127.0.0.1'
@@ -9,13 +10,21 @@ BUF_SIZE = 2048 # receive buffer size
 print('Opening socket.')
 clientSocket = socket(AF_INET, SOCK_DGRAM) # UDP socket
 
-print('Sending Hello to server.')
-clientSocket.sendto(b'Hello', (SERVER_NAME, SERVER_PORT)) # must be bytes
+def sendHello():
+    print('Sending Hello to server.')
+    clientSocket.sendto(b'Hello', (SERVER_NAME, SERVER_PORT)) # must be bytes
 
-response, serverAddress = clientSocket.recvfrom(BUF_SIZE)
-print(f'Received response from {serverAddress}: {response.decode()}')
+    response, serverAddress = clientSocket.recvfrom(BUF_SIZE)
+    print(f'Received response from {serverAddress}: {response.decode()}')
+
+CONTINUOUS = int(os.getenv('CONTINUOUS', '1'))
+if CONTINUOUS == 0: # non-continuous communication (send Hello before asking for name - to demonstrate timeout)
+    sendHello()
 
 name = input('Enter your name: ')
+
+if CONTINUOUS != 0: # continuous communication (to avoid timing out)
+    sendHello()
 
 print(f'Sending message "{name}" to server.')
 clientSocket.sendto(name.encode(), (SERVER_NAME, SERVER_PORT))
--- W4Credit/server.py
+++ W4Credit/server.py
@@ -1,4 +1,6 @@
 from socket import *
+import select
+import time
 
 # server listening IP and port
 SERVER_NAME = '' # IP address of interface to listen on, keep empty to listen on all network interfaces
@@ -8,23 +10,40 @@ BUF_SIZE = 2048 # receive buffer size
 
 print(f'Binding socket to port {SERVER_PORT}.')
 serverSocket = socket(AF_INET, SOCK_DGRAM) # UDP socket
+serverSocket.setblocking(False) # disable blocking (so we can run client expiry code while waiting for packet)
 serverSocket.bind((SERVER_NAME, SERVER_PORT))
 
-helloClients: set[tuple[str, int]] = set() # list of clients that sent Hello
+helloClients: dict[tuple[str, int], float] = dict() # list of clients that sent Hello
 # when the client has sent their name, we will remove them from the list (since communication is over at that stage)
 
+CLIENT_TIMEOUT = 10.0 # client timeout duration
+SELECT_TIMEOUT = 0.5 # timeout for select() call, so we don't end up sitting around forever waiting for data
+
 while True:
+    readSockets, _, _ = select.select([serverSocket], [], [], SELECT_TIMEOUT) # check if socket has data to be read
+    if serverSocket not in readSockets: # no data available - run client cleanup
+        currentTime = time.time()
+        removeClients = [] # list of clients to remove
+        for client in helloClients:
+            if helloClients[client] + CLIENT_TIMEOUT < currentTime: # this client timed out
+                print(f'Client {client} timed out')
+                removeClients.append(client)
+        for client in removeClients:
+            helloClients.pop(client) # finally remove the clients
+        continue # do not read from socket, otherwise there'll be errors        
+    
+    print('Reading from socket.')
     message, clientAddress = serverSocket.recvfrom(BUF_SIZE)
     messageStr = message.decode() # decode back to str
     print(f'Received message from {clientAddress}: {messageStr}')
     responseStr = ''
     if clientAddress not in helloClients: # initial communication - respond with "Hello, What's your name?"
         print('\t-> Hello message received.')
-        helloClients.add(clientAddress)
+        helloClients[clientAddress] = time.time()
         responseStr = 'Hello, What\'s your name?'
     else: # subsequent communication (name) - respond with "Hello {name}, Welcome to SIT202"
         print('\t-> Name message received.')
-        helloClients.remove(clientAddress)
+        helloClients.pop(clientAddress)
         responseStr = f'Hello {messageStr}, Welcome to SIT202'
 
     print(f'\t-> Response: {responseStr}')
